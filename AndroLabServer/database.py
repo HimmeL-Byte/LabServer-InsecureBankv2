24e2092a35727d7663485077dc7f7b99fba0060a
aexon <aexon@localhost.localdomain>
2025-02-21 19:26:25 +0700

    first commit

--- AndroLabServer/database.py
+++ AndroLabServer/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///mydb.db', convert_unicode=True)
+engine = create_engine('sqlite:///mydb.db')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -10,8 +10,9 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
+    # Import all modules here that might define models so that
+    # they will be registered properly on the metadata.
+    # Otherwise, you will have to import them first before calling init_db()
     import models
     Base.metadata.create_all(bind=engine)
+
